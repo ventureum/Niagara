547c59172b1dc0c32c7867ec0543b9b10cc70bf6
CHANGLONG ZHONG <vincentzhongca@gmail.com>
2018-08-15 10:59:06 -0400

    Resolve #188 Rename application

--- android/app/src/main/java/com/milestone/MainActivity.java
+++ android/app/src/main/java/com/milestone/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeseed;
+package com.milestone;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeSeed";
+        return "Milestone";
     }
 }
